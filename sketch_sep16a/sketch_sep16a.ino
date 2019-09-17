8a4acbbeb77a086718c5adb40f7ab13b6306a752
Jesper N. Pedersen <jeppenp12@gmail.com>
2019-09-17 09:27:20 +0200

    Firebase LED on/off

--- sketch_sep16a/sketch_sep16a.ino
+++ sketch_sep16a/sketch_sep16a.ino
@@ -1,73 +1,48 @@
-//Code for the www.MakeUseOf.com Wi-Fi connected Button tutorial by Ian Buckley
- 
-#include <IFTTTWebhook.h>
-#include <ESP8266WiFi.h>
- 
-#define ledPin 5
-#define wakePin 16
-#define ssid "Jesper P"
-#define password "metoo123"
-#define IFTTT_API_KEY "bze9-KISVD6x3rlquF1VQS"
-#define IFTTT_EVENT_NAME "pushbutton"
- 
+/* Controlling LED using Firebase console by CircuitDigest(www.circuitdigest.com) */
+#include <ESP8266WiFi.h>                                                // esp8266 library
+#include <FirebaseArduino.h>                                             // firebase library
+
+#define FIREBASE_HOST "bathroom-28f4f.firebaseio.com"                         // the project name address from firebase id
+#define FIREBASE_AUTH "m7uvInuGgG4hqqyI91vuwlQtnqYkC5zsfciGhvFF"                  // the secret key generated from firebase
+#define WIFI_SSID "Jesper P"                                         // input your home or public wifi name 
+#define WIFI_PASSWORD "metoo123"                                  //password of wifi ssid
+
+String fireStatus = "";                                                     // led status received from firebase
+int led = D1;                                                                // for external led
 void setup() {
   Serial.begin(9600);
-  while(!Serial) {
+  delay(1000);
+  pinMode(LED_BUILTIN, OUTPUT);      
+  pinMode(led, OUTPUT);                 
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);                                      //try to connect with wifi
+  Serial.print("Connecting to ");
+  Serial.print(WIFI_SSID);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
   }
-  Serial.println(" ");// print an empty line before and after Button Press    
-  Serial.println("Button Pressed");
-  Serial.println(" ");  
- 
-  connectToWifi();
- 
-  IFTTTWebhook hook(IFTTT_API_KEY, IFTTT_EVENT_NAME);
-  hook.trigger();
- 
-  pinMode(ledPin, OUTPUT);
-  digitalWrite(ledPin, HIGH);  
-  delay(200);              
-  digitalWrite(ledPin, LOW);
- 
- 
- 
- 
-  ESP.deepSleep(wakePin);
+  Serial.println();
+  Serial.print("Connected to ");
+  Serial.println(WIFI_SSID);
+  Serial.print("IP Address is : ");
+  Serial.println(WiFi.localIP());                                                      //print local IP address
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);                                       // connect to firebase
+  Firebase.setString("LED_STATUS", "OFF");                                          //send initial string of led status
 }
- 
- 
-void connectToWifi() {
-  // Serial.print("Connecting to: NAME "); //uncomment next line to show SSID name
-  Serial.print(ssid);
-  WiFi.begin(ssid, password);  
-  Serial.println(" ");
-  Serial.print("Attempting to connect: ");
- 
-  //try to connect for 10 seconds
-  int i = 10;
-  while(WiFi.status() != WL_CONNECTED && i >=0) {
-    delay(1000);
-    Serial.print(i);
-    Serial.print(", ");
-    i--;
+
+void loop() {
+  fireStatus = Firebase.getString("LED_STATUS");                                      // get ld status input from firebase
+  if (fireStatus == "ON") {                                                          // compare the input of led status received from firebase
+    Serial.println("Led Turned ON");                         
+    digitalWrite(LED_BUILTIN, LOW);                                                  // make bultin led ON
+    digitalWrite(led, HIGH);                                                         // make external led ON
+  } 
+  else if (fireStatus == "OFF") {                                                  // compare the input of led status received from firebase
+    Serial.println("Led Turned OFF");
+    digitalWrite(LED_BUILTIN, HIGH);                                               // make bultin led OFF
+    digitalWrite(led, LOW);                                                         // make external led OFF
   }
-  Serial.println(" ");
- 
-  //print connection result
-  if(WiFi.status() == WL_CONNECTED){
-  Serial.print("Connected.");
-  Serial.println(" ");
-  Serial.print("NodeMCU ip address: ");
-  Serial.println(WiFi.localIP());
+  else {
+    Serial.println("Wrong Credential! Please send ON/OFF");
   }
-  else{
-    Serial.println("Connection failed - check your credentials or connection");
-  }
-}
- 
- 
- 
-void loop(){
-//if deep sleep is working, this code will never run.
-Serial.println("This should never get printed");
-delay(1000);
 }
